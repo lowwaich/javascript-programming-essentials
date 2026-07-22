a5cf49b0ce81ffea59a054523fec483714e1f7d4
lowwaich <lowwaich@hotmail.com>
2026-07-22 03:36:08 -0400

    added Total Length

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -33,14 +33,17 @@ function endTest() {
             }).length;
 
             var wpm = 0; // Default value
+            var totalLength = 0;
 
             if (timeElapsed !== 0 && !isNaN(typedWords)) {
                 wpm = Math.round((typedWords / timeElapsed) * 60);
+                totalLength = userTypedText.length;
             }
 
             // Display the results
             var outputDiv = document.getElementById("output");
             outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+                "<p>Total Length: " + totalLength + "</p>" +
                 "<p>Words Typed: " + typedWords + "</p>" +
                 "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
                 "<p>Words Per Minute (WPM): " + wpm + "</p>";
