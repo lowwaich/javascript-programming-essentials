0903a0a3d8a04dad8ea142c5e76e31c90eec0724
lowwaich <lowwaich@hotmail.com>
2026-07-22 04:40:01 -0400

    added clearAllTasks

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -34,6 +34,12 @@ function clearCompletedTasks() {
             displayTasks();
         }
 
+function clearAllTasks() {
+    tasks = [];
+    displayTasks();
+}
+
 addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllTasksBtn.addEventListener("click", clearAllTasks);
 displayTasks();
